f58caea7ef576732fc97888ca203b5f6e4fb88b6
zakuro <z@kuro.red>
2022-08-10 15:03:04 +0900

    split Litsu.fs to Compiler.fs and Parser.fs

--- Litsu/Compiler.fs
+++ Litsu/Compiler.fs
@@ -4,6 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-open Litsu.Compiler
+module Litsu.Compiler
 
-"42" |> compile |> printfn "%s"
+open Litsu.Parser
+
+let compile code = parse code
