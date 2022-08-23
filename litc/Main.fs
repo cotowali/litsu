747f602304d9170ec5bae5fb796c85eec812c0ee
zakuro <z@kuro.red>
2022-08-23 13:12:24 +0900

    cli app wvith Argu

--- litc/Main.fs
+++ litc/Main.fs
@@ -4,8 +4,51 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+open Argu
+open System
+open System.IO
 open Litsu.Compiler
 open Litsu.Run
 
-"1 + 2 - 3 + 4 - 5" |> compile |> printfn "%s"
-run "1 + 2 - 3 + 4 - 5" |> printfn "%d"
+type Arguments =
+  | [<MainCommand>] File of file: string
+  | Run
+
+  interface IArgParserTemplate with
+    member this.Usage =
+      match this with
+      | File _ -> "input file"
+      | Run _ -> "run after compile"
+
+[<EntryPoint>]
+let main argv =
+  let p =
+    ArgumentParser.Create<Arguments>(
+      programName = "litc",
+      errorHandler =
+        ProcessExiter(
+          colorizer =
+            function
+            | ErrorCode.HelpText -> None
+            | _ -> Some ConsoleColor.Red
+        )
+    )
+
+  let results = p.ParseCommandLine argv
+
+  let code =
+    use file =
+      (match results.GetResults(Arguments.File) with
+       | [] -> Console.In
+       | [ path ] -> new StreamReader(path)
+       | _ -> eprintfn "too many input" |> exit 1)
+
+    file.ReadToEnd()
+
+  let isRun = List.contains Arguments.Run (results.GetResults(Arguments.Run))
+
+  if isRun then
+    run code
+  else
+    compile code |> printfn "%s"
+    0
